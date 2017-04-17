e0572f47f84261e27e1d9e6ec74dc106f7a6aa6a
bud aminof <budaminof@gmail.com>
2017-04-17 11:10:13 -0600

    refactor function to ES6 and changed var to const

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,28 +1,28 @@
 function Bob() {
 
-  var answers = {
+  const answers = {
     question: 'Sure.',
     yell: 'Whoa, chill out!',
     addresing: 'Fine. Be that way!',
     everythingElse: 'Whatever.'
   }
 
-  this.hey = function (term) {
+  this.hey = term => {
     if (this.silence(term)) return answers.addresing;
     if (this.yelling(term)) return answers.yell;
     if (this.isQuestion(term)) return answers.question;
     return answers.everythingElse;
   }
 
-  this.silence = function(term) {
+  this.silence = term => {
     return term.match(/^$/) || term.match(/^\s+$/) ? true : false;
   }
 
-  this.yelling = function(term) {
+  this.yelling = term => {
     return (term === term.toUpperCase() && (/[a-zA-Z]/g).test(term) === true) ? true : false;
   }
 
-  this.isQuestion = function(term) {
+  this.isQuestion = term => {
     return term[term.length-1] === "?" ? true : false;
   }
 
